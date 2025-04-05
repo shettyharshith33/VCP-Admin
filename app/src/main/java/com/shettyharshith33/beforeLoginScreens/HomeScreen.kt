87b8c5051eda5a819c677448f816d150733abc91
shettyharshith33 <harshithshetty033@gmail.com>
2025-04-05 11:28:39 +0530

    Modified the home screen and few images.

--- app/src/main/java/com/shettyharshith33/beforeLoginScreens/HomeScreen.kt
+++ app/src/main/java/com/shettyharshith33/beforeLoginScreens/HomeScreen.kt
@@ -4,31 +4,27 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
+import androidx.compose.ui.unit.sp
 import com.shettyharshith33.vcputtur.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -59,27 +55,67 @@ fun HomeScreen() {
         }
     }
 
+//
+//    Column(
+//        verticalArrangement = Arrangement.Top, modifier = Modifier
+//            .height(325.dp)
+//            .fillMaxWidth()
+//    ) {
+//        Box() {
+//            HorizontalPager(
+//                state = pagerState, modifier = Modifier.fillMaxSize()
+//            ) { page ->
+//                Image(
+//                    modifier = Modifier
+//                        .height(250.dp)
+//                        .fillMaxWidth()
+//                        .clip(RoundedCornerShape(45.dp))
+//                        .padding(top = 10.dp, bottom = 10.dp),
+//                    painter = painterResource(id = images[page]),
+//                    contentDescription = null
+//                )
+//            }
+//
+//        }
+//    }
 
-    Column(
-        verticalArrangement = Arrangement.Top, modifier = Modifier
-            .height(325.dp)
-            .fillMaxWidth()
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        Box() {
-            HorizontalPager(
-                state = pagerState, modifier = Modifier.fillMaxSize()
-            ) { page ->
-                Image(
-                    modifier = Modifier
-                        .height(250.dp)
-                        .fillMaxWidth()
-                        .clip(RoundedCornerShape(45.dp))
-                        .padding(top = 10.dp, bottom = 10.dp),
-                    painter = painterResource(id = images[page]),
-                    contentDescription = null
-                )
+        item {
+            Spacer(modifier = Modifier.height(25.dp))
+            Box(
+                modifier = Modifier
+                    .height(250.dp)
+                    .fillMaxWidth()
+            ) {
+                HorizontalPager(
+                    state = pagerState,
+                    modifier = Modifier.fillMaxSize()
+                ) { page ->
+                    Image(
+                        modifier = Modifier
+                            .height(250.dp)
+                            .fillMaxWidth()
+                            .clip(RoundedCornerShape(45.dp))
+                            .padding(top = 10.dp, bottom = 10.dp),
+                        painter = painterResource(id = images[page]),
+                        contentDescription = null
+                    )
+                }
             }
-
         }
+
+        item {
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "Category",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(start = 8.dp)
+            )
+        }
+
     }
+
 }
